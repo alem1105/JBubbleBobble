f80bddcc2507fa03e475dafcd5f4d0c3447b6459
Alessio Marini <marini.alessio1105@gmail.com>
2024-08-24 20:16:26 +0200

    -

---
+++ src/model/ui/buttons/QuitButtonModel.java
@@ -0,0 +1,9 @@
+package model.ui.buttons;
+
+public class QuitButtonModel extends CustomButtonModel{
+
+    public QuitButtonModel(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+}
