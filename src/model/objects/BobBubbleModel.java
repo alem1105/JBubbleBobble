6cbb549d6a72bbcb87983882c81c4989c5464c3a
Edoardo Noto <edo.noto8@gmail.com>
2024-08-25 22:14:34 +0200

    Aggiornamento alle bolle - Iniziata l'implementazione del movimento delle bolle

--- src/model/objects/BobBubbleModel.java
+++ src/model/objects/BobBubbleModel.java
@@ -10,7 +10,7 @@ import static model.utilz.Constants.GameConstants.TILES_SIZE;
 import static model.utilz.Gravity.*;
 
 public class BobBubbleModel extends BubbleModel {
-    private boolean justShot = true;
+    private int projectileTravelTimes = 0;
     private int targetTileY = 2;
 
     public BobBubbleModel(float x, float y, int width, int height, int bubbleDirection) {
@@ -22,16 +22,11 @@ public class BobBubbleModel extends BubbleModel {
     }
 
     private void updatePos() {
-//        if(justShot)
-//            firstShotMovement();
-//        else
-//            afterShotMovement();
-        if (bubbleDirection == RIGHT) {
-            hitbox.x+=bubbleSpeed;
+        if(projectileTravelTimes <= 60) {
+            firstShotMovement();
         }
-        else {
-            hitbox.x-=bubbleSpeed;
-        }
+        else
+            afterShotMovement();
     }
 
     private void afterShotMovement() {
@@ -39,28 +34,18 @@ public class BobBubbleModel extends BubbleModel {
     }
 
     private void firstShotMovement() {
-        for(int i = 0; i < (TILES_SIZE * 3) / bubbleSpeed; i++) {
-            if(bubbleDirection == RIGHT) {
-                hitbox.x += bubbleSpeed;
-            } else if(bubbleDirection == LEFT) {
-                hitbox.x -= bubbleSpeed;
-            }
-            /*
             if (bubbleDirection == RIGHT) {
-                if (CanMoveHere(x + bubbleSpeed, y, width, height, UtilityMethods.getLvlData()))
+                if (CanMoveHere(hitbox.x + bubbleSpeed, y, width, height, UtilityMethods.getLvlData()))
                     hitbox.x += bubbleSpeed;
                 else
                     hitbox.x = GetEntityXPosNextToWall(hitbox, bubbleSpeed);
             }else {
-                if (CanMoveHere(x - bubbleSpeed, y, width, height, UtilityMethods.getLvlData()))
+                if (CanMoveHere(hitbox.x - bubbleSpeed, y, width, height, UtilityMethods.getLvlData()))
                     hitbox.x -= bubbleSpeed;
                 else
                     hitbox.x = GetEntityXPosNextToWall(hitbox, bubbleSpeed);
             }
-            System.out.println(hitbox.x);
-             */
-        }
-        justShot = false;
+            projectileTravelTimes++;
     }
 
     private void changeDir() {
